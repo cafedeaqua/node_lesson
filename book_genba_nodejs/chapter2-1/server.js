d5d17c0d916627d4951be62c7cd2f198ae24eb0a
cafedeaqua <cafedeaqua@users.noreply.github.com>
2014-11-26 13:26:44 +0900

    Add chanter2-1 files

---
+++ book_genba_nodejs/chapter2-1/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer();
+server.on('request', doRequest);
+server.listen(1337);
+
+function doRequest(req, res){
+    fs.readFile('./index.html', 'UTF-8', doRead);
+
+    function doRead(err, data){
+	if(err) throw err;
+	res.setHeader('Content-Type', 'text/html');
+	res.write(data);
+	res.end();
+    }
+}
+
+console.log('Server running at http://127.0.0.1:1337');
+
