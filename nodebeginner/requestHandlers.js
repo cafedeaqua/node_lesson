2de9b9f76d207539b9fc5cead4a64b1552d0aa86
cafedeaqua <cafedeaqua@users.noreply.github.com>
2014-11-26 23:03:02 +0900

    Add image upload function

--- nodebeginner/requestHandlers.js
+++ nodebeginner/requestHandlers.js
@@ -1,25 +1,72 @@
 var child_process = require('child_process');
+var querystring = require("querystring")
+var fs = require("fs");
+var formidable = require('formidable');
 
-function start(response){
+function start(response, request){
     console.log("Request handler 'start' was called.");
-    var content = "empty";
-    child_process.exec("find /",
-		       { timeout:10000, maxBuffer: 20000*1024},
-		       function(err, stdout, stderr){
-	response.writeHead(200, {"Content-Type":"text/plain"});
-	response.write(stdout);
+    var body = '<html>'+
+	'<head>'+
+	'<meta http-equiv="Content-Type" content="text/html"'+
+	'charset=UTF-8 />'+
+	'</head>'+
+	'<body>'+
+	'<form action="/upload" enctype="multipart/form-data" method="post">'+
+	'<input type="file" name="upload" multiple="multiple" >'+
+	'<input type="submit" value="Upload file" />'+
+	'</form>'+
+	'</body>'+
+	'</html>';
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.write(body);
+    response.end();
+
+}
+
+function upload(response, request){
+    console.log("Request handler 'upload' was called.");
+
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error,fields, files){
+	console.log("parsing done");
+	console.log(error);
+	console.log(fields);
+	console.log(files);
+
+	/* Possible error on Windows system:
+	   tried to rename to an already existing file */
+	fs.rename(files.upload.path, "/tmp/test.png",function(err){
+	    if(err){
+		fs.unlink("/tmp/test.png");
+		fs.rename(files.upload.path, "/tmp/test.png");
+	    }
+	});
+	response.writeHead(200, {"Content-Type":"text/html"});
+	response.write("received image:<br/>");
+	response.write("<img src='/show' />");
 	response.end();
     });
 }
 
-function upload(response){
-    console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hello Upload");
-    response.end();
+function show(response, request){
+    console.log("Request handler 'show' was called.");
+    fs.readFile("/tmp/test.png", "binary", function(error,file){
+	if(error){
+	    response.writeHead(500,{"Content-Type":"text/plain"});
+	    response.write(error + "¥n");
+	    response.end();
+	}else{
+	    response.writeHead(200, {"Content-Type":"image/png"});
+	    response.write(file,"binary");
+	    response.end();
+	}
+    });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
+
 
 
--- nodebeginner/router.js
+++ nodebeginner/router.js
@@ -1,10 +1,12 @@
-function route(handle, pathname, response){
+function route(handle, pathname, response, request){
     console.log("Abount to route a request for " + pathname);
     if(typeof handle[pathname] === 'function' ){
-	return handle[pathname](response);
+	handle[pathname](response, request);
     }else{
 	console.log("No request handler found for " + pathname);
-	return "404 Not Found";
+	response.writeHead(404, {"Content-Type":"text/plain"});
+	response.write("404 Not Found");
+	response.end();
     }
 }
 
